0592d568d80c438ddf98ac9fd4394efc2fa4123a
alisson youssf <youssfbr@gmail.com>
2020-10-30 15:40:48 -0300

    Project created

---
+++ backend/src/test/java/com/alissondev/socialbook/SocialbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alissondev.socialbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
