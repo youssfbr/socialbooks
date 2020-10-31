d0ad9a6a8f4682554baac8d61b92d68848b4f956
alisson youssf <youssfbr@gmail.com>
2020-10-31 17:07:21 -0300

    Database seeding

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO tb_book (
+	name, publishing, publishing_company, summary, author)
+VALUES ('Java 2020', '2020-10-10', 'Saraiva', 'Conta a história do Java...', 'Alisson Youssf');
+
+INSERT INTO tb_book (
+	name, publishing, publishing_company, summary, author)
+VALUES ('Angular 9', '2020-10-17', 'Campos', 'Projeto em Angular', 'Robert Downer');
+
+INSERT INTO tb_book (
+	name, publishing, publishing_company, summary, author)
+VALUES ('Banco de Dados', '2019-05-04', 'Libriart', 'Projeto e fundamentos em Banco de Dados', 'Alceu Jr');
