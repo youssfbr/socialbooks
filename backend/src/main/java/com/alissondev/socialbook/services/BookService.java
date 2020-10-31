91d59610ba604db9f346b4202b0270fbc7d85be8
alisson youssf <youssfbr@gmail.com>
2020-10-31 13:55:59 -0300

    @Transactional

--- backend/src/main/java/com/alissondev/socialbook/services/BookService.java
+++ backend/src/main/java/com/alissondev/socialbook/services/BookService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.alissondev.socialbook.entities.Book;
 import com.alissondev.socialbook.repositories.BookRepository;
@@ -14,6 +15,7 @@ public class BookService {
 	@Autowired
 	private BookRepository repository;
 	
+	@Transactional(readOnly = true)
 	public List<Book> findAll() {
 		return repository.findAll();
 	}
