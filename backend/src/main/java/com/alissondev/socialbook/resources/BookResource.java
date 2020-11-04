4586483cdfa78ff2fc97783c6923ec4b19f5176f
alisson youssf <youssfbr@gmail.com>
2020-11-04 12:00:18 -0300

    Book insert

--- backend/src/main/java/com/alissondev/socialbook/resources/BookResource.java
+++ backend/src/main/java/com/alissondev/socialbook/resources/BookResource.java
@@ -1,13 +1,17 @@
 package com.alissondev.socialbook.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.alissondev.socialbook.dto.BookDTO;
 import com.alissondev.socialbook.services.BookService;
@@ -27,5 +31,15 @@ public class BookResource {
 	@GetMapping("/{id}")
 	public ResponseEntity<BookDTO> findById(@PathVariable Long id) {
 		return ResponseEntity.ok(service.findById(id));
+	}
+	
+	@PostMapping
+	public ResponseEntity<BookDTO> insert(@RequestBody BookDTO dto) {
+		dto = service.insert(dto);	
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri()
+				.path("/{id}").buildAndExpand(dto.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(dto);
 	}
 }
--- backend/src/main/java/com/alissondev/socialbook/services/BookService.java
+++ backend/src/main/java/com/alissondev/socialbook/services/BookService.java
@@ -29,4 +29,16 @@ public class BookService {
 		Book book = repository.findById(id).orElseThrow(() -> new NotFoundException("Id " + id + " not found"));
 		return new BookDTO(book);
 	}
+
+	@Transactional
+	public BookDTO insert(BookDTO dto) {
+		
+		Book entity = new Book();
+		entity.setName(dto.getName());
+		entity.setPublishingCompany(dto.getPublishingCompany());
+		entity.setAuthor(dto.getAuthor());				
+		entity = repository.save(entity);
+		
+		return new BookDTO(entity);
+	}
 }
